1936ab90caf105ca15ebfc38d35140c85994fde9
sirdan <stanford303@gmail.com>
2022-05-11 11:03:05 -0700

    removed comments

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 import { renderListItem } from './render.js';
 import { planets } from './planets.js';
 
-// import functions and grab DOM elements
 
-// let state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 const solar = document.getElementById('solar');
 for (let planet of planets) {
     const renderPlanet = renderListItem(planet);
